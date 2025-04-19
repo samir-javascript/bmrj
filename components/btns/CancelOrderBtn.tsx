7842da21799cd9baba77b5ebebe758baf3c697e8
samir-javascript <samirjavascript00@gmail.com>
2025-04-19 19:15:21 +0200

    orders btns ui

--- components/btns/CancelOrderBtn.tsx
+++ components/btns/CancelOrderBtn.tsx
@@ -8,7 +8,7 @@ const CancelOrderBtn = () => {
     <>
          <Button onClick={() => setOpen(true)} className="h-fit py-1.5 flex-1 border border-light_blue bg-transparent
                       hover:bg-transparent text-light_blue max-sm:w-fit rounded-full text-xs font-bold">
-                        annuler la commande
+                        annuler <br className="max-sm:block hidden" /> la commande
          </Button>
          <CancelOrderModal open={open} setOpen={setOpen} />
     </>
