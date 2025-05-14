f451a3939ed1c7562835a8ada7dd668be515d3d1
samir-javascript <samirjavascript00@gmail.com>
2025-05-14 02:05:06 +0200

    test loading state

---
+++ app/(admin)/admin/dashbord/loading.tsx
@@ -0,0 +1,12 @@
+import LoadingAppState from '@/components/Loaders/LoadingAppState'
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div>
+         <LoadingAppState />
+    </div>
+  )
+}
+
+export default loading
