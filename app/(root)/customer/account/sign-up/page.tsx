b418a72f3dc361c8df509f0a0937fe868a63e9b5
samir-javascript <samirjavascript00@gmail.com>
2025-05-15 03:37:42 +0200

    z10

--- app/(root)/customer/account/sign-up/page.tsx
+++ app/(root)/customer/account/sign-up/page.tsx
@@ -15,10 +15,10 @@ const page = async() => {
    
     
     return (
-       <div className='relative flex items-center justify-center min-h-screen px-4 py-10 !overflow-x-hidden'>
+       <div className='relative flex items-center justify-center min-h-screen px-4 py-10  !overflow-x-hidden'>
   {/* Blurred background image */}
   <div className="absolute inset-0 bg-auth-image bg-center bg-cover bg-no-repeat blur-sm scale-105 z-0"></div>
-        <div className='sm:shadow-md shadow-none rounded-lg px-5 py-4 flex flex-col gap-3 mx-auto lg:min-w-[600px] '>
+        <div className='sm:shadow-md z-10 shadow-none rounded-lg px-5 py-4 flex flex-col gap-3 mx-auto lg:min-w-[600px] '>
             <div className='w-[60px] h-[60px] flex mx-auto text-center
              items-center justify-center border-2 border-light_blue rounded-full '>
                 <User className='text-light_blue' size={30} />
