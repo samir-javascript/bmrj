9f5b907ed9448e943fc45e98d75dedc744f83c8a
samir-javascript <samirjavascript00@gmail.com>
2025-05-17 02:06:34 +0200

    wis

--- components/btns/AddAllWishlistBtn.tsx
+++ components/btns/AddAllWishlistBtn.tsx
@@ -5,18 +5,23 @@ import { Button } from '../ui/button'
 import { CollectionElement } from '@/types/Elements'
 import { addWishlistToCart } from '@/lib/store/cartSlice'
 import LoadingAppState from '../Loaders/LoadingAppState'
+import { useToast } from '@/hooks/use-toast'
 
 const AddAllWishlistBtn = ({items}: {
      items: CollectionElement[]
 }) => {
     // Explicitly type dispatch to accept thunks
     const dispatch = useAppDispatch()
+    const {toast} = useToast()
     const [loading,setLoading] = useState(false)
     const handleAddWishlistItemsToCart = async()=> {
         setLoading(true)
         try {
             // @ts-ignore
           dispatch(addWishlistToCart(items));
+         return toast({
+             title: "Success"
+          })
           //TODO:  deleteWishlistItems
         } catch (error) {
              console.log(error)
@@ -31,6 +36,7 @@ const AddAllWishlistBtn = ({items}: {
     )}
         <Button
       type='button'
+
       className='rounded-2xl max-sm:text-xs bg-light_blue text-white hover:bg-light_blue '
       onClick={handleAddWishlistItemsToCart}
     >
