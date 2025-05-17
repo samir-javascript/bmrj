ab0db03a823ab0e4b1b2de97471f694b6f6f5852
samir-javascript <samirjavascript00@gmail.com>
2025-05-18 00:55:57 +0200

    loading

--- app/(root)/search/loading.tsx
+++ app/(root)/search/loading.tsx
@@ -1,11 +1,12 @@
 import LoadingAppState from '@/components/Loaders/LoadingAppState'
+import SearchSkeleton from '@/components/skeletons/SearchSkeleton'
 import React from 'react'
 
 const loading = () => {
     // TODO: add loading skeleton
   return (
     <div>
-         <LoadingAppState />
+         <SearchSkeleton />
     </div>
   )
 }
---
+++ components/skeletons/SearchSkeleton.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Skeleton } from '../ui/skeleton'
+
+const SearchSkeleton = () => {
+  return (
+   <div className="flex flex-col space-y-3">
+        <Skeleton className="h-4 bg-gray-200 w-[350px]" />
+        <Skeleton className="h-4 bg-gray-200 w-[300px]" />
+    <div className='mt-5 w-full mx-auto'>
+        
+ <div className="flex items-center mx-auto flex-wrap max-sm:gap-1 gap-3">
+                      {[0,1,2,3,4,5,6,7,8,9,10,11,22,58,67].map((_,index) => (
+                         <div key={index}>
+                               <Skeleton className='w-[180px] max-sm:w-[165px] h-[330px] rounded-lg ' />
+                         </div>
+                     ))}
+                     </div>
+         
+          {/* {result.data?.collection.length! > 0 && <Pagination page={Number(page) || 1} isNext={result.data?.isNext as boolean} />} */}
+        </div>
+    
+    </div>
+  )
+}
+
+export default SearchSkeleton
