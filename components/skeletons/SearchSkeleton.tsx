1e6b8ae369afb03c50e102b1179a404aff0a6fbd
samir-javascript <samirjavascript00@gmail.com>
2025-05-18 01:25:26 +0200

    k

--- components/skeletons/SearchSkeleton.tsx
+++ components/skeletons/SearchSkeleton.tsx
@@ -3,10 +3,10 @@ import { Skeleton } from '../ui/skeleton'
 
 const SearchSkeleton = () => {
   return (
-   <div className="flex flex-col space-y-3">
+   <div className="flex flex-col space-y-3 py-8">
         <Skeleton className="h-4 bg-gray-200 w-[350px]" />
         <Skeleton className="h-4 bg-gray-200 w-[300px]" />
-    <div className=' w-full '>
+    
         
  <div className="flex items-center mx-auto flex-wrap max-sm:gap-1 gap-3">
                       {[0,1,2,3,4,5,6,7,8,9,10,11,22,58,67].map((_,index) => (
@@ -17,7 +17,7 @@ const SearchSkeleton = () => {
                      </div>
          
           {/* {result.data?.collection.length! > 0 && <Pagination page={Number(page) || 1} isNext={result.data?.isNext as boolean} />} */}
-        </div>
+       
     
     </div>
   )
