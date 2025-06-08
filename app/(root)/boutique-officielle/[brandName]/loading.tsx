f07bdb24d291ee79caa08fc6d922ee5b6c938afd
samir-javascript <samirjavascript00@gmail.com>
2025-06-09 00:50:56 +0200

    skeleton

---
+++ app/(root)/boutique-officielle/[brandName]/loading.tsx
@@ -0,0 +1,10 @@
+import CategoryPageSkeleton from '@/components/skeletons/CategoryPageSkeleton'
+import React from 'react'
+
+const loading = () => {
+  return (
+    <CategoryPageSkeleton />
+  )
+}
+
+export default loading
---
+++ components/skeletons/CategoryPageSkeleton.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Skeleton } from '../ui/skeleton'
+
+const CategoryPageSkeleton = () => {
+  return (
+   <div className="flex flex-col space-y-4 py-8 px-4 lg:px-8 max-w-7xl mx-auto">
+      {/* Heading skeletons */}
+      <Skeleton className="h-4 w-[350px] bg-gray-200" />
+      <Skeleton className="h-[400px] w-full rounded-lg bg-gray-200" />
+       
+      {/* Product cards skeleton */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 pt-4">
+        {Array.from({ length: 15 }).map((_, index) => (
+          <Skeleton
+            key={index}
+            className="h-[280px] w-full bg-gray-200 rounded-lg"
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default CategoryPageSkeleton
