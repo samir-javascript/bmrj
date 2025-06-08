cec1c7c39c96e4cacdf1c1e727bd77a733973358
samir-javascript <samirjavascript00@gmail.com>
2025-06-08 18:14:11 +0200

    jh

--- app/(root)/category/[categoryName]/page.tsx
+++ app/(root)/category/[categoryName]/page.tsx
@@ -23,8 +23,8 @@ const page = async({params}: {params: Promise<{categoryName:string}>}) => {
              </p>
         </div>
          <CategoryImage categoryName={categoryName}  />
-         <div className='px-3 flex flex-col space-x-7'>
-            <h2 className='h2-bold'>Notre sélection</h2>
+         <div className='px-3 flex mt-5 flex-col space-x-7'>
+            <h2 className='h2-bold mb-3'>Notre sélection</h2>
              <div>
           {data?.products.length! > 0 ? (
            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 pt-4">
