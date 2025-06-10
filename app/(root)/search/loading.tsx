79d7f233e33d0e9e9003d6cdf35092b2453648f2
samir-javascript <samirjavascript00@gmail.com>
2025-06-11 01:25:37 +0200

    j

---
+++ app/(root)/search/loading.tsx
@@ -0,0 +1,12 @@
+import SearchSkeleton from '@/components/skeletons/SearchSkeleton'
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div>
+        <SearchSkeleton />
+    </div>
+  )
+}
+
+export default loading
