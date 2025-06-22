240e32a07ad849c3eef368ade08de73a5de6909e
samir-javascript <samirjavascript00@gmail.com>
2025-06-23 00:48:35 +0200

    fi

--- components/btns/SaveProductHeart.tsx
+++ components/btns/SaveProductHeart.tsx
@@ -6,7 +6,7 @@ import { Heart } from "lucide-react"
 import { useSession } from "next-auth/react";
 import { startTransition, use, useOptimistic, useState } from "react";
 import { AuthModal } from "../modals/AuthModal";
-import useMobile from "@/hooks/use-mobile";
+
 
 import { useToast } from "@/hooks/use-toast";
 
@@ -22,57 +22,57 @@ const SaveProductHeart = ({className,productId,hasSavedProductPromise}: {
     )
     
     const [open,setOpen] = useState<boolean>(false)
-    const isMobile = useMobile()
+   
    
     const session = useSession()
     const {toast} = useToast()
  
-   const handleToggleSaveProduct = async () => {
-      if (session.status !== "loading" && session.status !== "authenticated") {
-        setOpen(true)
-        return
-      }
+  //  const handleToggleSaveProduct = async () => {
+  //     if (session.status !== "loading" && session.status !== "authenticated") {
+  //       setOpen(true)
+  //       return
+  //     }
     
-      // ✅ Optimistically update in a transition
-      startTransition(() => {
-        toggleOptimisticSaved(!optimisticSaved)
-      })
+  //     // ✅ Optimistically update in a transition
+  //     startTransition(() => {
+  //       toggleOptimisticSaved(!optimisticSaved)
+  //     })
     
      
     
-      try {
-        const { success, error } = await toggleSaveCollection({ productId })
+  //     try {
+  //       const { success, error } = await toggleSaveCollection({ productId })
     
-        if (success) {
-         //  toast({
-         //    title: "Success",
-         //    description: optimisticSaved
-         //      ? "Item has been removed from your wishlist"
-         //      : "Item has been added to your wishlist",
-         //  })
-        } else {
-          toast({
-            title: "Error",
-            description: error?.message,
-            variant: "destructive",
-          })
+  //       if (success) {
+  //        //  toast({
+  //        //    title: "Success",
+  //        //    description: optimisticSaved
+  //        //      ? "Item has been removed from your wishlist"
+  //        //      : "Item has been added to your wishlist",
+  //        //  })
+  //       } else {
+  //         toast({
+  //           title: "Error",
+  //           description: error?.message,
+  //           variant: "destructive",
+  //         })
     
-          // ⛔ Revert if failed
-          startTransition(() => {
-            toggleOptimisticSaved(optimisticSaved)
-          })
-        }
-      } catch (error) {
-        console.error(error)
-        startTransition(() => {
-          toggleOptimisticSaved(optimisticSaved)
-        })
-      }
-    }
+  //         // ⛔ Revert if failed
+  //         startTransition(() => {
+  //           toggleOptimisticSaved(optimisticSaved)
+  //         })
+  //       }
+  //     } catch (error) {
+  //       console.error(error)
+  //       startTransition(() => {
+  //         toggleOptimisticSaved(optimisticSaved)
+  //       })
+  //     }
+  //   }
     
     console.log(hasSavedProductPromise, "over here")
   return (
-    <div onClick={handleToggleSaveProduct} className={`rounded-full flex  w-[45px] h-[45px] bg-gray-100  items-center justify-center ${className}`}>
+    <div onClick={()=> {}} className={`rounded-full flex  w-[45px] h-[45px] bg-gray-100  items-center justify-center ${className}`}>
                   {optimisticSaved ? (
   <img className="w-[30px] cursor-pointer object-contain" src="/red-heart.png" />
 ) : (
